cebc55c9cc67dc8b16134b1206600b765a47e0f9
hy <hyplayer001@gmail.com>
2026-06-03 21:21:50 +0800

    fix: remove figma preview bar

--- frontend/components/layout/PageWrapper.jsx
+++ frontend/components/layout/PageWrapper.jsx
@@ -1,11 +1,9 @@
-import FigmaBar from "./FigmaBar";
 import Footer from "./Footer";
 import Navbar from "./Navbar";
 
 export default function PageWrapper({ children }) {
   return (
     <div className="page-shell">
-      <FigmaBar />
       <Navbar />
       {children}
       <Footer />
